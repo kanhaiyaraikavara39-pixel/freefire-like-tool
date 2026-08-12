cf6358175ef0640ac147ab10353037ca92e6c2fa
kanhaiyaraikavara39-pixel <kanhaiyaraikavara39@gmail.com>
2026-08-12 22:25:41 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="ff_like_api",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
-    description="Free Fire Custom Like API Library",
-    author="ZEXXY (Your Name)",
+    description="Free Fire Custom Like API Library - One command setup",
+    author="ZEXXY",
     install_requires=[
-        "Flask",
-        "requests",
-        "aiohttp",
-        "pycryptodome",
-        "protobuf",
-        "Werkzeug"
+        "Flask>=2.0.0",
+        "requests>=2.25.0",
+        "aiohttp>=3.7.0",
+        "pycryptodome>=3.10.0",
+        "protobuf>=3.20.0",
+        "Werkzeug>=2.0.0",
     ],
+    entry_points={
+        "console_scripts": [
+            "ff-like-server=ff_like_api.server:start_server",
+        ],
+    },
+    python_requires=">=3.7",
 )
